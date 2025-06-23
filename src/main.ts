752b98cddde8b2a4b8a88f0bf1a4ec3040f3e973
Alonso Chavarria <alons182@hotmail.com>
2025-06-23 16:13:03 -0600

    Update Android Gradle Wrapper and project configuration
    - Added gradle-wrapper.properties to specify Gradle version 8.11.1.
    - Introduced gradlew and gradlew.bat scripts for Gradle execution on POSIX and Windows systems respectively.
    - Configured settings.gradle to include app and capacitor-cordova-android-plugins modules.
    - Created variables.gradle to define project-wide variables including SDK versions and library dependencies.

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router';
 
-import { IonicVue } from '@ionic/vue';
+import { alertController, IonicVue } from '@ionic/vue';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/vue/css/core.css';
@@ -33,6 +33,7 @@ import '@ionic/vue/css/palettes/dark.system.css';
 
 /* Theme variables */
 import './theme/variables.css';
+import OneSignal, { NotificationClickEvent, NotificationWillDisplayEvent } from 'onesignal-cordova-plugin';
 
 const app = createApp(App)
   .use(IonicVue)
@@ -40,4 +41,64 @@ const app = createApp(App)
 
 router.isReady().then(() => {
   app.mount('#app');
+
+  OneSignal.initialize("1ebd3376-bf83-40bd-9172-9435856f28d1");
+  // Use this method to prompt for push notifications.
+  // We recommend removing this method after testing and instead use In-App Messages to prompt for notification permission.
+  OneSignal.Notifications.requestPermission();
+  // OneSignal.Notifications.requestPermission(false).then((accepted: boolean) => {
+  //   console.log("User accepted notifications: " + accepted);
+  // });
+
+  const interval = setInterval(async () => {
+
+    const id = await OneSignal.User.getOnesignalId();
+    console.log('OnesignalId', id);
+
+    if (id) {
+      clearInterval(interval);
+      /// logica para guardar el id en la base de datos
+    }
+
+  }, 60000);
+
+
+  const myClickListener = async function (event: NotificationClickEvent) {
+    const notificationData = JSON.stringify(event);
+    console.log(notificationData);
+    presentAlert(event.notification.body);
+   
+  };
+
+  OneSignal.Notifications.addEventListener("click", myClickListener);
+
+
+
+    const displayNotification = (event: NotificationWillDisplayEvent) => {
+    // Use preventDefault() to not display
+    // event.preventDefault();
+    // Use notification.display() to display the notification after some async work
+    event.getNotification().display();
+
+    console.log("OneSignal notification received:", JSON.stringify(event.getNotification()));
+     presentAlert(event.getNotification().body);
+   
+   
+}
+OneSignal.Notifications.addEventListener("foregroundWillDisplay", displayNotification);
+
+
+
 });
+
+
+async function presentAlert(message:string) {
+  const alert = await alertController.create({
+    header: 'Notificacion',
+    message,
+    buttons: ['OK'],
+  });
+
+  await alert.present();
+}
+
